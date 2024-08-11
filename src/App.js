6308b8ade1253358e84abc8109fb1a66d9894088
Dusty <mashnunurrahman@gmail.com>
2024-08-11 23:52:42 +0600

    added canvas for test

--- src/App.js
+++ src/App.js
@@ -30,15 +30,18 @@ function App() {
         ctx.stroke()
       }    
     }
-
+    /*git add .
+    git commit -m "added canvas for test"
+    git push*/
     function animate(){
       ctx.clearRect(0, 0, canvas.width, canvas.height)
 
       for(let star = 0; star < stars.length; star++){
         if(stars[star].x > canvas.width || stars[star].x > canvas.height){
-          stars[star].x = Math.random() * canvas.width
-          stars[star].y = Math.random() * canvas.height
+          stars[star].x = (Math.random() - 1) * canvas.width
+          stars[star].y = (Math.random() - 1) * canvas.height
           stars[star].len = 80 + Math.random() * 60
+          stars[star].speed = (Math.random() * 2) + 2
         }else {
           stars[star].x += stars[star].speed 
           stars[star].y += stars[star].speed 
@@ -49,8 +52,8 @@ function App() {
       requestAnimationFrame(animate)
     }
 
-    for(let i = 0; i < 8; i++){
-      stars.push({x: Math.random() * canvas.width, y: Math.random() * canvas.height, len: 80 + Math.random() * 60, speed: 4})
+    for(let i = 0; i < 4; i++){
+      stars.push({x: (Math.random() - 1) * canvas.width, y: (Math.random() + 1) * canvas.height, len: 80 + Math.random() * 60, speed: (Math.random() * 2) + 2})
     }
    
     console.log(stars)
