091b75ede34493f26c016afb6807721b4f8919ce
Dusty <mashnunurrahman@gmail.com>
2024-08-12 22:09:20 +0600

    added canvas for test

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ export default function Home(){
         const ctx = canvas.getContext(`2d`)
         
         canvas.width = window.innerWidth
+        console.log(canvas.width)
         canvas.height = window.innerHeight
         const stars = []
         
@@ -30,19 +31,43 @@ export default function Home(){
             ctx.stroke()
           }    
         }
-        let radius = 100 
-        function animate(){
-           
-          ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        
+        let iterated = 0
+        const dampen = 0.8
+        const rotSpeed = 0.04 * dampen
+        const radDistance = Math.PI / 2
+        
+        function animate(){          
+          ctx.clearRect(0, 0, canvas.width, canvas.height)  
           ctx.beginPath()
-          ctx.arc(208, 378, radius, 0, Math.PI*2*0.33)
-          ctx.lineWidth = 8
-          ctx.strokeStyle = `#50AFFF`
+          ctx.lineWidth = 2
+          ctx.lineCap = `round`
+          ctx.strokeStyle = `#004374`
+          ctx.shadowBlur = 0
+          ctx.arc(canvas.width / 2, canvas.height / 2, 90, Math.PI + rotSpeed * iterated, Math.PI + rotSpeed * iterated + radDistance)
+          ctx.stroke() 
+          ctx.beginPath() 
+          ctx.arc(canvas.width / 2, canvas.height / 2, 90, rotSpeed * iterated, rotSpeed * iterated + radDistance)
+          ctx.stroke()  
+          ctx.beginPath()
+          ctx.lineWidth = 4
+          ctx.arc(canvas.width / 2, canvas.height / 2, 96, 4 * Math.PI / 3 - rotSpeed * iterated, 2 * Math.PI - rotSpeed * iterated)
           ctx.stroke()
-          radius++
+          ctx.beginPath()
+          ctx.arc(canvas.width / 2, canvas.height / 2, 96, Math.PI / 3 - rotSpeed * iterated, Math.PI - rotSpeed * iterated)
+          ctx.stroke()
+          ctx.beginPath()
+          ctx.lineWidth = 6
+          ctx.arc(canvas.width / 2, canvas.height / 2, 105, 0 + rotSpeed * iterated, Math.PI + rotSpeed * iterated)
+          ctx.stroke()
+          ctx.beginPath()
+          ctx.arc(canvas.width / 2, canvas.height / 2, 105, 4 * Math.PI / 3 + rotSpeed * iterated, 5 * Math.PI / 3 + rotSpeed * iterated)
+          ctx.stroke()
+          iterated++
     
           for(let star = 0; star < stars.length; star++){
-            if(stars[star].x > canvas.width || stars[star].x > canvas.height){
+            if(stars[star].x > canvas.width || stars[star].y > canvas.height){
               stars[star].x = (Math.random() - 1) * canvas.width
               stars[star].y = (Math.random() - 1) * canvas.height
               stars[star].len = 80 + Math.random() * 60
@@ -58,8 +83,6 @@ export default function Home(){
         }
 
         
-        console.log(ctx)
-    
         for(let i = 0; i < 4; i++){
           stars.push({x: (Math.random() - 1) * canvas.width, y: (Math.random() + 1) * canvas.height, len: 80 + Math.random() * 60, speed: (Math.random() * 2) + 2})
         }
@@ -78,6 +101,10 @@ export default function Home(){
                 </div>
             </div>
             <div className="homeBody" >
+                <div className="introduction">
+                    <h1>Hi, I'm Mashnun</h1>
+                    <span>Front-end Developer</span>
+                </div>
                 <img src="/favicon.ico" alt=""/>
             </div>
         </div>
